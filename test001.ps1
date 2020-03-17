93691e31f3e233080fbd1ed2ad3caa66755454aa
ShortArrow <bamboogeneral@live.jp>
2020-03-18 08:59:05 +0900

    to7

--- test001.ps1
+++ test001.ps1
@@ -1,13 +1,16 @@
+. .\Select-Folder.ps1
+
+
+
 # Set-PSDebug -Trace 0
 $7zpath="C:\Program Files\7-Zip\7z.exe"
 $x001=(Get-ChildItem)
 [System.IO.FileInfo]$y001
 foreach ($y001 in $x001) {
-    if (("",".ps1",".cmd",".zip",".7z") -notcontains $y001.Extension -and !($y001.PSIsContainer)) {
-        $z001=$(Join-Path $y001.Directory $($([System.IO.Path]::GetFileNameWithoutExtension("`"$y001`""))+".zip"))
-        Start-Process -FilePath $7zpath -ArgumentList 'a', "-tzip", "`"$z001`"", "`"$y001`""
-        Write-Host $(Join-Path $y001.Directory $($([System.IO.Path]::GetFileNameWithoutExtension($y001))+".zip"))
-        
+    if (("",".ps1",".cmd",".zip",".7z") -notcontains $y001.Extension -and !($y001.PSIsContainer) -and ($y001.Extension -ne $y001.Name)) {
+        $z001=$(Join-Path $y001.Directory $($([System.IO.Path]::GetFileNameWithoutExtension("`"$y001`""))+".7z"))
+        Start-Process -FilePath $7zpath -ArgumentList 'a', "-t7z", "`"$z001`"", "`"$y001`""
+        Write-Host $z001" <-- "$y001
     }
 }
 

4468aaf4908932f6c05087939f45e0ae93b922b0
ShortArrow <bamboogeneral@live.jp>
2020-03-20 01:37:50 +0900

    add password option

--- test001.ps1
+++ test001.ps1
@@ -1,6 +1,10 @@
+Param(
+    [Parameter()]
+    [ValidatePattern("[a-zA-Z0-9@!$%&=\-^~|_/?><.,+;:*}{]+")]
+    [SecureString]
+    $Password   
+)
 . .\Select-Folder.ps1
-
-
 
 # Set-PSDebug -Trace 0
 $7zpath="C:\Program Files\7-Zip\7z.exe"
@@ -9,7 +13,11 @@ $x001=(Get-ChildItem)
 foreach ($y001 in $x001) {
     if (("",".ps1",".cmd",".zip",".7z") -notcontains $y001.Extension -and !($y001.PSIsContainer) -and ($y001.Extension -ne $y001.Name)) {
         $z001=$(Join-Path $y001.Directory $($([System.IO.Path]::GetFileNameWithoutExtension("`"$y001`""))+".7z"))
-        Start-Process -FilePath $7zpath -ArgumentList 'a', "-t7z", "`"$z001`"", "`"$y001`""
+        if ($Password -eq $null) {
+            Start-Process -FilePath $7zpath -ArgumentList 'a', "-t7z", "`"$z001`"", "`"$y001`"" -NoNewWindow
+        }else {
+            Start-Process -FilePath $7zpath -ArgumentList 'a', "-t7z", "`"$z001`"", "-p"$Password, "-mhe", "`"$y001`"" -NoNewWindow
+        }
         Write-Host $z001" <-- "$y001
     }
 }
